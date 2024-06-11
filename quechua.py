f5d84c757723efd18f7042cfdf1fab9f5cdb7fb0
ValeriaP10 <a20196166@pucp.edu.pe>
2024-06-11 13:10:52 -0500

    hola

--- quechua.py
+++ quechua.py
@@ -192,8 +192,7 @@ if base and numero and persona and tiempo:
     resultado = conj_final(base, numero, persona, tiempo)
     if resultado:
         st.write("El verbo conjugado es: ")
-        with st.container():
-            st.markdown(f'<p style="font-size:24px; text-align:center;">{resultado}</p>', unsafe_allow_html=True)
+        st.markdown(f'<p style="font-size:24px; text-align:center;">{resultado}</p>', unsafe_allow_html=True)
 else:
     st.error("Por favor, asegúrese de que todas las opciones estén seleccionadas.")
 
