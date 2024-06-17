571aeba5add94efabceeafe2e751580774459eee
ValeriaP10 <a20196166@pucp.edu.pe>
2024-06-17 13:02:43 -0500

    hola

--- quechua.py
+++ quechua.py
@@ -95,7 +95,7 @@ from PIL import Image
 
 image = Image.open('mapa.jpg')
 
-st.image(image, caption='Mapa de Ayacucho', width=250, use_column_width=True)
+st.image(image, caption='Mapa de Ayacucho', use_column_width=True, width=250, )
 
 ########### menú desplegable para seleccionar VERBOS #################
 
