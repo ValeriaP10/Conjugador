d4bea2bcf41387806b4d9dfdec951fb9abcb7cf9
ValeriaP10 <a20196166@pucp.edu.pe>
2024-06-24 16:25:16 -0500

    hola

--- quechua.py
+++ quechua.py
@@ -107,7 +107,7 @@ st.markdown(
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
     .title-font {
-        font-family: 'Pacifico';
+        font-family: 'Patrick Hand';
         color: purple;
         text-align: center;
         font-size: 50px;
